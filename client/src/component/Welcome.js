68c620e5396c92ae49b60ca503f7a288d54ed26f
Wynne-Tran <wynnetran2018@gmail.com>
2022-05-09 02:02:48 -0400

    Add back-end

--- client/src/component/Welcome.js
+++ client/src/component/Welcome.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { AiFillAlipayCircle } from 'react-icons/ai'
 import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle } from 'react-icons/bs'
 import { Loader } from './Index';
+import { TransactionContext } from '../context/TransactionContext';
+
 
 const commonStyle = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white'
 
@@ -17,9 +19,9 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
     />
 )
 const Welcome = () => {
-    const connectWallet = () => {
-
-    }
+    const {connectWallet} = useContext(TransactionContext);
+    console.log(connectWallet);
+   
 
     const handleSubmit = () => {
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
+
+   
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { TransactionProvider } from "./context/TransactionContext";
+import "./index.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <TransactionProvider>
     <App />
-  </React.StrictMode>
+  </TransactionProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
